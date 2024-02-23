11de2cb1e16d4c881463beda1855e12cb33981d1
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-02-22 21:08:40 -0300

    commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 fetch("./version.json")
     .then(response => response.json())
-    .then(data => document.head.querySelector("meta[name='version']").innerHTML = JSON.stringify(data));
+    .then(data => document.head.querySelector("meta[name='version']").value = JSON.stringify(data));
