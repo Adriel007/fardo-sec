f4a23a2d64129bf8118d0a8122c9c2e32c33e65b
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-02-22 22:53:20 -0300

    commit

--- tools/menu.js
+++ tools/menu.js
@@ -3,5 +3,5 @@ const askQuestion = require('./askQuestion');
 module.exports = async() => {
     console.clear();
     console.log("Fardo Sec - Ataque de Segurança");
-    require(`../sec/${await askQuestion(`Escolha uma opção:\n- help\n- ssh\n- swarm\n- exit`)}`)();
+    require(`../sec/${await askQuestion(`Menu:\n- help\n- ssh\n- swarm\n- exit\n\n\nEscolha uma opção:`)}`)();
 };
