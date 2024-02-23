df970c28767b891b8a264471fb21618707d46226
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-02-22 21:32:46 -0300

    commit

--- fardo-sec.js
+++ fardo-sec.js
@@ -1,4 +1,16 @@
-const { exec } = require('child_process');
+const fs = require('fs');
 
+fs.readFile('./index.html', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Erro ao ler o arquivo:', err);
+    return;
+  }
+  
+  data.split("\n").forEach(line => {
+    if (line.includes('"version": ')) {
+      console.log(line.split('"')[3])
+    }
+  });
+});
 
 require('./tools/menu')();
--- index.js
+++
@@ -1,3 +0,0 @@
-fetch("./version.json")
-    .then(response => response.json())
-    .then(data => document.head.querySelector("meta[name='version']").content = JSON.stringify(data));
