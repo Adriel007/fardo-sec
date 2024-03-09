9f18582ec0977030358cc16472cf810490ee131b
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-03-09 12:04:50 -0300

    commit

--- tools/glitch.sh
+++ tools/glitch.sh
@@ -3,20 +3,20 @@ FONT_RED="\e[31m"
 FONT_RESET="\e[0m"
 
 terminal_width=$(tput cols)
-
+text=$(cat ./fardo-sec/art/glitch.txt)
 spaces=$(( (terminal_width - ${#text}) / 2 ))
 
 clear
-printf "%*s%s%*s\n" $spaces "" "cat ./fardo-sec/art/glitch.txt" $spaces ""
+printf "%*s%s\n" $spaces '' "$(echo "$text")"
 
 read -p "Press any key to start..."
 
 for i in {9..1}; do
     clear
     if [ $((i % 2)) -eq 1 ]; then
-        printf "%*s%s%*s\n" $spaces "" "cat ./fardo-sec/art/glitch.txt" $spaces ""
+        printf "%*s%s\n" $spaces '' "$(echo "$text")"
     else
-        printf "%*s${FONT_RED}%s${FONT_RESET}%*s\n" $spaces "" "cat ./fardo-sec/art/glitch.txt" $spaces ""
+        printf "%*s${FONT_RED}%s${FONT_RESET}%*s\n" $spaces "" "$text" $spaces ""
     fi
     sleep 0.$i
 done
