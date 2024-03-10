39f005d574a3805f68df61298d388bfe0617a166
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-03-09 21:13:27 -0300

    commit

--- tools/create_alias.sh
+++ tools/create_alias.sh
@@ -4,6 +4,6 @@ FONT_RESET="\e[0m"
 clear 
 echo "To create an alias, scroll to the bottom and write:"
 echo "alias <name>=\"<command>\""
-echo -e "\n${FONT_GREEN}BE CAREFUL, DO NOT DELETE OTHER LINES${FONT_RESET}"
+echo -e "\n${FONT_RED}BE CAREFUL, DO NOT DELETE OTHER LINES${FONT_RESET}"
 read -p "Press ENTER to continue..."
 nano /data/data/com.termux/files/usr/etc/bash.bashrc"
