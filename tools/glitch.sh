4cc20fede323d39187f96d5c229fda9d5b8ddfa9
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-03-10 08:49:46 -0300

    x

--- tools/glitch.sh
+++ tools/glitch.sh
@@ -3,12 +3,25 @@ FONT_GREEN="\e[32m"
 FONT_RESET="\e[0m"
 art=$(<$HOME/fardo-sec/art/interrogation.txt)
 
-echo "$art"
+rows=$(tput lines)
+cols=$(tput cols)
+
+row_offset=$(( (rows / 2) - 1 ))
+col_offset=$(( (cols - ${#art}) / 2 ))
+
+clear
+
+tput cup $row_offset $col_offset
+echo -e "$art"
+
 read -p "Press ENTER to start GLITCH"
 
 for i in {5..1}; do
     for ((j=0; j<4; j++)); do
         clear
+
+        tput cup $row_offset $col_offset
+
         if [ $((j % 2)) -eq 0 ]; then
             echo -e "${FONT_RESET}$art"
         else
