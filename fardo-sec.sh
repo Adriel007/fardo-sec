6f9934b43ba420bd4c53438dd3e496d33fbdc77f
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-03-09 21:09:54 -0300

    commit

--- fardo-sec.sh
+++ fardo-sec.sh
@@ -36,9 +36,8 @@ menu() {
         "chmod +x ./tools/glitch.sh && ./tools/glitch.sh"
         "echo 'Broker choice'"
         "echo 'Wlan-F'"
-        "echo 'clear && To create alias, scroll to bottom and write: && echo alias <name>=\"<command>\"' && echo -e \"echo -e "\n${FONT_GREEN}BE CAREFUL, DO NOT DELETE OTHERS LINES${FONT_RESET}\" && read -p "Press ENTER to continue..." && nano /data/data/com.termux/files/usr/etc/bash.bashrc"
+        "chmod +x ./tools/create_alias.sh && ./tools/create_alias.sh"
         "source /data/data/com.termux/files/usr/etc/bash.bashrc && clear && echo Alias updated"
-    )
 
     echo $(pwd)
     echo -e "${FONT_RED}$(figlet Fardo - Sec)${FONT_RESET}"
---
+++ tools/create_alias.sh
@@ -0,0 +1,6 @@
+clear 
+echo "To create an alias, scroll to the bottom and write:"
+echo "alias <name>=\"<command>\""
+echo -e "\n${FONT_GREEN}BE CAREFUL, DO NOT DELETE OTHER LINES${FONT_RESET}"
+read -p "Press ENTER to continue..."
+nano /data/data/com.termux/files/usr/etc/bash.bashrc"
