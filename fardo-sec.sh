efd8f9f03ff79f262df4437fba07f95abfe30cbb
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-03-10 07:46:03 -0300

    x

--- fardo-sec.sh
+++ fardo-sec.sh
@@ -58,7 +58,8 @@ menu() {
         echo -e "${FONT_GREEN}$((i+1))${FONT_RESET}) ${options[$i]}"
     done
 
-    echo -e "\n${FONT_GREEN}0${FONT_RESET}) Exit\n\n"
+    echo -e "\n${FONT_GREEN}0${FONT_RESET}) Exit"
+    echo -e "${FONT_RED}X${FONT_RESET}) Uninstall\n\n"
 
     read -p "Your choice: " choice
 
@@ -68,6 +69,13 @@ menu() {
         menu
     elif [[ "$choice" -eq 0 ]]; then
         clear
+        exit 0
+    elif [[ "$choice" -eq "x" ]]; then
+        clear
+        rm -rf $HOME/fardo-sec
+        rm $HOME/.shortcuts/glitch.sh
+        sed -i "/alias fardo-sec='\$HOME\/fardo-sec\/fardo-sec.sh'/d" /data/data/com.termux/files/usr/etc/bash.bashrc
+        source /data/data/com.termux/files/usr/etc/bash.bashrc
         exit 0
     else
         menu
