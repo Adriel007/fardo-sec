31c30d374dd3186d144456cb48daffb68d3c58d6
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-03-13 13:30:28 -0300

    commit

---
+++ animation.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+diretorio=$0
+
+# Contagem inicial de arquivos .txt
+total=$(ls -1 "$diretorio"/*.txt 2>/dev/null | wc -l)
+
+# Loop para iterar sobre os arquivos .txt
+while true; do
+    for ((i = 0; i < total; i++)); do
+        arquivo="$diretorio/$i.txt"
+        if [ -f "$arquivo" ]; then
+            cat "$arquivo"
+            sleep 0.2
+            clear
+        fi
+    done
+done
