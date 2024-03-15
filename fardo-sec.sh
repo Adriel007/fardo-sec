18d9ec03aa6c645527059c6a0ee5b204a3558c99
Adriel007 <adrielsouzaandrade8@gmail.com>
2024-03-15 20:43:46 -0300

    x

--- fardo-sec.sh
+++ fardo-sec.sh
@@ -87,7 +87,7 @@ menu() {
     elif [[ "$choice" -eq 0 ]]; then
         clear
         exit 0
-    elif [[ "$choice" == "K" ]]; then
+    elif [[ "$choice" == "K" || "$choice" == "k" ]]; then
         if [ "$keyboard" = false ]; then
             keyboard=true
             echo "Keyboard mode enabled, delay between commands set to 2 seconds"
